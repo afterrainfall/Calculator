7c3cd58da18a94a1c55b78bf93d33040e0593d52
afterrain <98341037+afterrainfall@users.noreply.github.com>
2023-09-20 01:19:39 +0800

    Store values and operator in proper variables

--- calculator.js
+++ calculator.js
@@ -24,17 +24,18 @@ let optr = '';
 // operate function
 
 function operate(optr, num1, num2){
-    operation[optr](num1, num2);
+    return operation[optr](num1, num2);
 }
 
 
 // add value container
+// add display
 
 let valueArray = [];
+let operands = '';
 
 
 let displayValue = document.createElement('div');
-
 document.body.appendChild(displayValue);
 
 let display = document.body.querySelector('.calculator-display');
@@ -44,22 +45,75 @@ display.textContent = '0';
 
 // add buttons presses to display
 
-buttons.forEach(button => button.addEventListener('click', function(e) {
-    
+let i = 1;
+
+buttons.forEach(button => button.addEventListener('click', function(e) {    
+
     if (valueArray.length < 12 && !this.classList.contains("optr")) {
+        // if (valueArray[1] == optr) {
+        //     display.textContent = '';
+        //     valueArray.push(this.textContent);
+        //     valueArray = valueArray.join('');
+        //     console.log(valueArray);
+        //     display.textContent = valueArray[2];
+        //     num2 = +valueArray[2];
+        //     console.log(valueArray[2]);
+        //     console.log(num2);
+        // } else {
         valueArray.push(this.textContent);
-        display.textContent = valueArray.join('');
+        valueArray = valueArray.join('');
+        display.textContent = valueArray;
+        switch (i % 2) {
+            case (1): 
+            num1 = +valueArray;
+            break;
+
+            case (0): 
+            num2 = +valueArray;
+            break;
+        }
+        valueArray = valueArray.split('');
+        console.log(num1);
+        console.log(num2);
+      
+        // }
     }
 
-    displayValue.textContent = valueArray.join('');
+    // displayValue.textContent = valueArray.join('');
 
     if (this.classList.contains("optr")) {
-        display.textContent = '';
-
+        valueArray = valueArray.join('').split(this.textContent);
+        console.log(valueArray);
+        valueArray.splice(0,2);
         optr = this.textContent;
+        console.log(optr);
+        // valueArray[1] = '';
+        // valueArray[1] = this.textContent;
+        // valueArray = valueArray.join('');
+        console.log(valueArray);
+        console.log(num1);
+        console.log(num2);
+        i++;
     }
 
+    // if (valueArray[1]) {
+    //     display.textContent = '';
+    //     valueArray.push(this.textContent);
+    //     console.log(valueArray);
+    // }
+
+    //     valueArray.push(this.textContent);
+    //     operands = valueArray.join('').split(`+`);
+    //     console.log(operands);
+    //     num1 = operands[0];
+    //     optr = operands[1];
+    //     console.log(optr);
+    //     console.log(num1);
+    // }
+
 }));
+
+
 
 
 
