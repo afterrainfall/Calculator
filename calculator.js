0a480666ca32d9b1edc0f1024581147bff6e2d74
afterrain <98341037+afterrainfall@users.noreply.github.com>
2023-09-19 18:44:50 +0800

    Add values on display

--- calculator.js
+++ calculator.js
@@ -14,6 +14,13 @@ operation = {
 }
 
 
+// variable for each num
+
+let num1 = 0;
+let num2 = 0;
+let optr = '';
+
+
 // operate function
 
 function operate(optr, num1, num2){
@@ -21,13 +28,39 @@ function operate(optr, num1, num2){
 }
 
 
-// add buttons presses to display
+// add value container
+
+let valueArray = [];
+
+
+let displayValue = document.createElement('div');
+
+document.body.appendChild(displayValue);
+
 let display = document.body.querySelector('.calculator-display');
 let buttons = document.body.querySelectorAll('button');
 display.textContent = '0';
 
+
+// add buttons presses to display
+
 buttons.forEach(button => button.addEventListener('click', function(e) {
-    display.textContent = this.textContent;
-    // console.log(this.textContent);
-})
-);
+    
+    if (valueArray.length < 12 && !this.classList.contains("optr")) {
+        valueArray.push(this.textContent);
+        display.textContent = valueArray.join('');
+    }
+
+    displayValue.textContent = valueArray.join('');
+
+    if (this.classList.contains("optr")) {
+        display.textContent = '';
+
+        optr = this.textContent;
+    }
+
+}));
+
+
+
+
